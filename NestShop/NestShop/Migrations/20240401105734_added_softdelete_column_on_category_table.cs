387b33ffba2da5772cfaaa427b1d1dc4172e02ea
Nihattaghizade <nihattagizade2003@gmail.com>
2024-04-04 00:40:50 +0400

    d

---
+++ NestShop/NestShop/Migrations/20240401105734_added_softdelete_column_on_category_table.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NestShop.Migrations
+{
+    public partial class added_softdelete_column_on_category_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SoftDeleted",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SoftDeleted",
+                table: "Categories");
+        }
+    }
+}
